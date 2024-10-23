58a6cfb858028921e95ff940779e476fa08d120b
HoangThaiDev <thaindev2000@gmail.com>
2024-10-23 21:56:05 +0700

    Upload deploy

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { API_ROOT } from "./utils/constants";
+
+function App() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      try {
+        const res = await axios.get(`${API_ROOT}/shop/products`, {
+          proxy: 1,
+          withCredentials: true,
+        });
+
+        if (res.statusText === "OK") {
+          const products = res.data;
+
+          setProducts(products);
+          setIsLoading(true);
+        }
+      } catch (error) {
+        console.log("API Context Error:", error);
+        setIsLoading(false);
+      }
+    };
+    fetchProduct();
+  }, []);
+  return (
+    <div className="App">
+      <h1>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rem, est.
+        Cupiditate a atque laboriosam ab praesentium nostrum molestias,
+        similique sed architecto repellat minima consectetur quo accusantium
+        perspiciatis unde quae deleniti.
+      </h1>
+      {isLoading && (
+        <div>
+          {products.map((p) => (
+            <img src={p.images[0]} alt="" key={p._id} />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/utils/constants.js
@@ -0,0 +1,13 @@
+import env from "./enviroment";
+console.log(env);
+
+let apiRootServer = "";
+
+if (env.BUILD_MODE === "development") {
+  apiRootServer = env.URL_SERVER_DEV;
+}
+if (env.BUILD_MODE === "production") {
+  apiRootServer = env.URL_SERVER_PRODUCTION;
+}
+
+export const API_ROOT = apiRootServer;
